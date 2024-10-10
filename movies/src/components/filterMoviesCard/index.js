9351332a91a66e9adcac73ebe9eec02e04820ca8
Moonquakes <Zhuanz@MacBook-Air.local>
2024-10-10 20:56:40 +0100

    Added filtering of movie list - Movies lab 1 completed

--- movies/src/components/filterMoviesCard/index.js
+++ movies/src/components/filterMoviesCard/index.js
@@ -49,6 +49,7 @@ export default function FilterMoviesCard(props) {
     
       const handleChange = (e, type, value) => {
         e.preventDefault()
+        props.onUserInput(type, value)
         // Completed later
       };
 
